72753072162b0956467892ecc2cad4dafc97d55b
linhnguyen26 <linhnlnse140957@fpt.edu.vn>
2021-11-28 00:43:27 +0700

    Scaffoding CRUD API

---
+++ Entity/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManagement.Entity
+{
+    [Table("Book")]
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Book name must not be empty")]
+        [StringLength(1000, MinimumLength = 3, ErrorMessage = "Book name length must between 3 and 1000")]
+        public String BookName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Category must not be empty")]
+        [StringLength(1000, MinimumLength = 3, ErrorMessage = "Category length must between 3 and 1000")]
+        public String Category { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
+        public int Quantity { get; set; }
+    }
+}
---
+++ Migrations/20211127173626_Init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookManagement.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BookName = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
